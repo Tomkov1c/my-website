d09d7dd3055edc00a514969a34bff127b358c70e
Tomkov1c <tomkliner9@gmail.com>
2023-11-18 17:35:30 +0100

    Added a projects page. Needs some more JavaScript

--- buttons.js
+++ buttons.js
@@ -63,3 +63,10 @@ function AboutSectionRem() {
     GetCurrentSection();
 
 }
+function ProjectsSectionRem() {
+    document.getElementById("body").classList.remove("bodyProjects");
+    document.location.hash = "#";
+
+    GetCurrentSection();
+
+}
